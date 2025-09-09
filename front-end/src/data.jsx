ffc93341e30565355865f25dcd1d7e8dad2a2b96
LOKESH PARDHI <165950254+Tech-Lksh@users.noreply.github.com>
2025-09-09 07:11:50 +0000

    Movie App

---
+++ front-end/src/data.jsx
@@ -0,0 +1,23 @@
+let movieList = [
+  "Inception",
+  "The Dark Knight",
+  "Interstellar",
+  "Avatar",
+  "Titanic",
+//   "Avengers: Endgame",
+//   "Spider-Man: No Way Home",
+//   "Joker",
+//   "Black Panther",
+//   "Frozen II"
+];
+
+let slotsList = [
+  "09:00 AM",
+  "12:00 PM",
+  "03:00 PM",
+  "06:00 PM",
+  "09:00 PM"
+];
+
+const _movieList = movieList;
+export { _movieList as movieList };
